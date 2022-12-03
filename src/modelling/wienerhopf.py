8607f390e59c41288388c519c6661147b3153841
Gabriel Lara <gtlchaves@gmail.com>
2022-12-03 12:46:24 -0300

    Adiciona metodo de wiener hopf

---
+++ src/modelling/wienerhopf.py
@@ -0,0 +1,31 @@
+import numpy as np
+from src.modelling.deconvolution import get_h
+from src.modelling.utils import create_convolution_matrix, get_h
+
+
+def get_autocorr(signal: np.ndarray) -> np.ndarray:
+
+    autocorr = get_crosscorr(signal, signal)
+
+    return autocorr
+
+
+def get_crosscorr(signal_a: np.ndarray,
+                  signal_b: np.ndarray) -> np.ndarray:
+
+    crosscorr = get_crosscorr(signal_a, signal_b)
+
+    return crosscorr
+
+
+def estimate_h_by_wiener_hopf(input_signal: np.ndarray,
+                              output_signal: np.ndarray,
+                              n_samples: int = None) -> np.ndarray:
+
+    autocorr_input = get_autocorr(input_signal)
+    crosscorr_input_output = get_crosscorr(input_signal, output_signal)
+
+    conv_matrix = create_convolution_matrix(autocorr_input, size=n_samples)
+    h = get_h(crosscorr_input_output, conv_matrix)
+
+    return h
