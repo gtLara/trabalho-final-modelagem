9a1a5357b1f67a54ddf3dbff9dddae168f861d37
Gabriel Lara <gtlchaves@gmail.com>
2022-12-04 12:02:34 -0300

    Adiciona comparacao de respostas

--- estimate.py
+++ estimate.py
@@ -10,7 +10,7 @@ sig_range = range(0, 599)
 
 for label, signals in io_pair.items():
     input_signal = signals["input"].reshape(-1)[sig_range]
-    impulse_signal = signals["impulse"].reshape(-1)[sig_range]
+    impulse_signal = signals["impulse"][:, 0][sig_range][:100]
     output_signal = signals["output"][:, 0][sig_range]
     plt.plot(input_signal, label="input")
     plt.plot(output_signal, label="output")
@@ -19,10 +19,18 @@ for label, signals in io_pair.items():
     plt.show()
     h_deconv = estimate_h_by_deconv(input_signal, output_signal)[:100]
     h_wiener = estimate_h_by_wiener_hopf(input_signal, output_signal)[:100]
-    plt.plot(impulse_signal[:100], label="deconv")
+
+    plt.plot(impulse_signal, label="impulso verdadeiro")
     plt.title(label)
+    plt.legend()
     plt.show()
-    plt.plot(h_deconv, label="wiener")
+
+    plt.plot(h_deconv, label="deconv")
+    plt.title(label)
+    plt.legend()
+    plt.show()
+
     plt.plot(h_wiener, label="wiener")
     plt.title(label)
+    plt.legend()
     plt.show()
