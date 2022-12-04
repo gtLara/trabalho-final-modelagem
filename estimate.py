5c764f59baf47eb0ed3150dbbdd9b261cfe8902e
Gabriel Lara <gtlchaves@gmail.com>
2022-12-04 11:53:50 -0300

    Adiciona verificacao de sinais

--- estimate.py
+++ estimate.py
@@ -5,15 +5,20 @@ from src.utils.noise import add_noise
 from src.modelling.deconvolution import estimate_h_by_deconv
 from src.modelling.wienerhopf import estimate_h_by_wiener_hopf
 
-io_pair = get_data("data/impulse", val=False)
+io_pair = get_data("data/estimation", val=False)
 sig_range = range(0, 599)
 
 for label, signals in io_pair.items():
     input_signal = signals["input"].reshape(-1)[sig_range]
     impulse_signal = signals["impulse"].reshape(-1)[sig_range]
     output_signal = signals["output"][:, 0][sig_range]
-    h_deconv = estimate_h_by_deconv(input_signal, output_signal)
-    h_wiener = estimate_h_by_wiener_hopf(input_signal, output_signal)
+    plt.plot(input_signal, label="input")
+    plt.plot(output_signal, label="output")
+    plt.title(label)
+    plt.legend()
+    plt.show()
+    h_deconv = estimate_h_by_deconv(input_signal, output_signal)[:100]
+    h_wiener = estimate_h_by_wiener_hopf(input_signal, output_signal)[:100]
     plt.plot(impulse_signal[:100], label="deconv")
     plt.title(label)
     plt.show()
